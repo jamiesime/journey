9a9385d32aeee99f182978e2fff0a7496e53b468
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-12-15 10:35:05 +0000

    Event should have a description -- test passing

--- client/src/specs/event_specs.js
+++ client/src/specs/event_specs.js
@@ -7,13 +7,15 @@ describe( "Event", function(){
 
   beforeEach(function(){
 
-    testEvent = new Event("Battle of Montgomery's Tavern");
+    testEvent = new Event("Battle of Montgomery's Tavern", "The Battle of Montgomery's Tavern was an incident in the Upper Canada Rebellion. The abortive revolutionary insurrection inspired by William Lyon Mackenzie was crushed by British authorities and Canadian volunteer units near a tavern on Yonge Street, Toronto.");
   });
 
   it("should have a name", function(){
     assert.strictEqual(testEvent.name, "Battle of Montgomery's Tavern")
   });
-  xit("should have a description");
+  it("should have a description", function(){
+    assert.strictEqual(testEvent.description, "The Battle of Montgomery's Tavern was an incident in the Upper Canada Rebellion. The abortive revolutionary insurrection inspired by William Lyon Mackenzie was crushed by British authorities and Canadian volunteer units near a tavern on Yonge Street, Toronto.")
+  });
   xit("should have a date");
   xit("should have a choices array")
 
