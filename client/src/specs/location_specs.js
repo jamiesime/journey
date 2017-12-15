a1d64c155303fc026da4d98c18d6f91aa46928e2
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-12-15 11:38:40 +0000

    InfoView refactor

--- client/src/specs/location_specs.js
+++ client/src/specs/location_specs.js
@@ -6,7 +6,7 @@ describe( "Location", function(){
   var testLocation
 
   beforeEach(function(){
-    testLocation = new Location("Nova Scotia", [44.651070, -63.582687], "Galic name for New Scotland", ["Event A", "Event B"]);
+    testLocation = new Location("Nova Scotia", [44.651070, -63.582687], "Gaelic name for New Scotland", ["Event A", "Event B"]);
   });
 
   it("should have a name", function(){
@@ -19,11 +19,10 @@ describe( "Location", function(){
     assert.deepStrictEqual(testLocation.latlng[1], -63.582687)
   });
   it("should have a description", function(){
-    assert.strictEqual(testLocation.description, "Galic name for New Scotland")
+    assert.strictEqual(testLocation.description, "Gaelic name for New Scotland")
   })
   it("should have events", function(){
     assert.strictEqual(testLocation.events.length, 2)
   });
-
 
 });
