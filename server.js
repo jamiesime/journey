00d8f2da4aa2081ec2173a38b7dfd9173593739f
Jamie Sime <jamiesime@gmail.com>
2017-12-15 10:29:47 +0000

    defined db in server as client.db(journey)

--- server.js
+++ server.js
@@ -14,10 +14,19 @@ MongoClient.connect("mongodb://localhost:27017/journey", function(err, client){
   if (err){
     return console.log(err);
   }
-  db = client.db('bucketList');
+  db = client.db('journey');
   console.log("connected to db");
 
   app.listen(3000, function(){
     console.log("Listening on port 3000");
+  });
+});
+
+app.get("/", function(req, res){
+  db.collection("journey").find().toArray(function(err, results){
+    if (err){
+      return console.log(err);
+    }
+    res.json(results);
   });
 });
