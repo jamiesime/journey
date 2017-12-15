424333bed38b3a60954032dda37892cf234df8ec
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-12-15 11:20:03 +0000

    Added semi colons to the event class test and class

---
+++ client/src/event.js
@@ -0,0 +1,58 @@
+var Event = function(name, description, date, choices) {
+  this.name = name;
+  this.description = description;
+  this.date = date;
+  this.choices = choices;
+}
+
+Event.prototype.getMonth = function(monthNumber){
+  switch(monthNumber) {
+    case 1:
+    return "January";
+    break;
+    case 2:
+    return "February"
+    break;
+    case 3:
+    return "March";
+    break;
+    case 4:
+    return "April";
+    break;
+    case 5:
+    return "May";
+    break;
+    case 6:
+    return "June";
+    break;
+    case 7:
+    return "July";
+    break;
+    case 8:
+    return "August"
+    break;
+    case 9:
+    return "September";
+    break;
+    case 10:
+    return "October";
+    break;
+    case 11:
+    return "November";
+    break;
+    case 12:
+    return "December";
+    break;
+  }
+}
+
+Event.prototype.getDateAsString = function(date){
+  date = new Date(date * 1000);
+  var day = date.getDay();
+  var month = this.getMonth(date.getMonth()+1);
+  var year = date.getFullYear();
+  var fullYearString = day + " " + month + ", " + year;
+  return fullYearString;
+}
+
+module.exports = Event;
---
+++ client/src/specs/event_specs.js
@@ -0,0 +1,47 @@
+var assert = require("assert");
+var Event = require("../event.js");
+
+describe( "Event", function(){
+
+  var testEvent;
+
+  beforeEach(function(){
+    testEvent = new Event(
+      "Battle of Montgomery's Tavern",
+      "The Battle of Montgomery's Tavern was an incident in the Upper Canada Rebellion. The abortive revolutionary insurrection inspired by William Lyon Mackenzie was crushed by British authorities and Canadian volunteer units near a tavern on Yonge Street, Toronto.",
+      -4167676800,
+      ["Stay and Fight", "Flee to America with your Family"]);
+    });
+
+    it("should have a name", function(){
+      assert.strictEqual(testEvent.name, "Battle of Montgomery's Tavern");
+    });
+
+    it("should have a description", function(){
+      assert.strictEqual(testEvent.description, "The Battle of Montgomery's Tavern was an incident in the Upper Canada Rebellion. The abortive revolutionary insurrection inspired by William Lyon Mackenzie was crushed by British authorities and Canadian volunteer units near a tavern on Yonge Street, Toronto.");
+    });
+
+    it("should have a date", function(){
+      var eventDate = testEvent.getDateAsString(testEvent.date)
+      assert.strictEqual(eventDate, "4 December, 1837");
+    });
+
+    it("should have a choices array", function(){
+      assert.deepStrictEqual(testEvent.choices, ["Stay and Fight", "Flee to America with your Family"]);
+    });
+
+    it("should be able to return a month name by number", function(){
+      var monthName = testEvent.getMonth(1);
+      assert.strictEqual(monthName, "January");
+    });
+
+    it("should be able to return a month name by number", function(){
+      var monthName = testEvent.getMonth(1);
+      assert.strictEqual(monthName, "January");
+    });
+
+    it("should be able to turn a unix time object into a date string", function(){
+      var eventDate = testEvent.getDateAsString(testEvent.date);
+      assert.strictEqual(eventDate, "4 December, 1837");
+    });
+  });
