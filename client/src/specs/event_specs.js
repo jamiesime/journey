beb4b7cd220dbe929e37d831dee6adaf64440275
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-12-15 10:38:07 +0000

    Event should have a date -- test passing

--- client/src/specs/event_specs.js
+++ client/src/specs/event_specs.js
@@ -7,19 +7,24 @@ describe( "Event", function(){
 
   beforeEach(function(){
 
-    testEvent = new Event("Battle of Montgomery's Tavern", "The Battle of Montgomery's Tavern was an incident in the Upper Canada Rebellion. The abortive revolutionary insurrection inspired by William Lyon Mackenzie was crushed by British authorities and Canadian volunteer units near a tavern on Yonge Street, Toronto.");
-  });
+    testEvent = new Event(
+      "Battle of Montgomery's Tavern",
+      "The Battle of Montgomery's Tavern was an incident in the Upper Canada Rebellion. The abortive revolutionary insurrection inspired by William Lyon Mackenzie was crushed by British authorities and Canadian volunteer units near a tavern on Yonge Street, Toronto.",
+      "7 December, 1837");
+    });
 
-  it("should have a name", function(){
-    assert.strictEqual(testEvent.name, "Battle of Montgomery's Tavern")
-  });
-  it("should have a description", function(){
-    assert.strictEqual(testEvent.description, "The Battle of Montgomery's Tavern was an incident in the Upper Canada Rebellion. The abortive revolutionary insurrection inspired by William Lyon Mackenzie was crushed by British authorities and Canadian volunteer units near a tavern on Yonge Street, Toronto.")
-  });
-  xit("should have a date");
-  xit("should have a choices array")
+    it("should have a name", function(){
+      assert.strictEqual(testEvent.name, "Battle of Montgomery's Tavern")
+    });
+    it("should have a description", function(){
+      assert.strictEqual(testEvent.description, "The Battle of Montgomery's Tavern was an incident in the Upper Canada Rebellion. The abortive revolutionary insurrection inspired by William Lyon Mackenzie was crushed by British authorities and Canadian volunteer units near a tavern on Yonge Street, Toronto.")
+    });
+    it("should have a date", function(){
+      assert.strictEqual(testEvent.date, "7 December, 1837")
+    });
+    xit("should have a choices array")
 
- // function(){
- //    assert.strictEqual(recordStore.name, "Big Al's Records");
- //  });
-});
+    // function(){
+    //    assert.strictEqual(recordStore.name, "Big Al's Records");
+    //  });
+  });
