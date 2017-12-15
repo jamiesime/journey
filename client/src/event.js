bb4c4d334370ed4addbbcbb90fa2d69dde944ef6
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-12-15 10:30:55 +0000

    Event class constructor and module.exports setup

---
+++ client/src/event.js
@@ -0,0 +1,8 @@
+var Event = function(name, description, date, choices) {
+  this.name = name;
+  this.description = description;
+  this.date = date;
+  this.choices = choices;
+}
+
+module.exports = Event;
