01398e9012a7ef0c6fdac4ea159d106aedee61da
LouiseReid <l_reid@hotmail.co.uk>
2017-12-15 10:27:26 +0000

    location tests set up

---
+++ client/src/specs/location_specs.js
@@ -0,0 +1,19 @@
+var assert = require("assert")
+var event = require("../location.js")
+
+describe( "Location", function(){
+
+  var testLocation
+
+  beforeEach(function(){
+    testLocation = new Location();
+  });
+
+  it("should have a name");
+  it("should have a lat");
+  it("should have a lng");
+  it("should have a description")
+  it("should have events");
+
+
+});
