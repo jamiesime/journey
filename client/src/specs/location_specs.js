2b3ac0490db0a84c0cb7e5b99b58f9dc151d8517
LouiseReid <l_reid@hotmail.co.uk>
2017-12-15 10:37:34 +0000

    location lat test passing

--- client/src/specs/location_specs.js
+++ client/src/specs/location_specs.js
@@ -6,13 +6,15 @@ describe( "Location", function(){
   var testLocation
 
   beforeEach(function(){
-    testLocation = new Location("Nova Scotia");
+    testLocation = new Location("Nova Scotia", [5, 3]);
   });
 
   it("should have a name", function(){
     assert.strictEqual(testLocation.name, "Nova Scotia")
   });
-  it("should have a lat");
+  it("should have a lat", function(){
+    assert.deepStrictEqual(testLocation.latlng[0], 5)
+  });
   it("should have a lng");
   it("should have a description")
   it("should have events");
