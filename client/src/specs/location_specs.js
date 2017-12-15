f54caeaa1653a1930d08a094925bfd0fdfdeed09
LouiseReid <l_reid@hotmail.co.uk>
2017-12-15 10:40:15 +0000

    location description test passing

--- client/src/specs/location_specs.js
+++ client/src/specs/location_specs.js
@@ -6,7 +6,7 @@ describe( "Location", function(){
   var testLocation
 
   beforeEach(function(){
-    testLocation = new Location("Nova Scotia", [5, 3]);
+    testLocation = new Location("Nova Scotia", [5, 3], "Galic name for New Scotland");
   });
 
   it("should have a name", function(){
@@ -18,7 +18,9 @@ describe( "Location", function(){
   it("should have a lng", function(){
     assert.deepStrictEqual(testLocation.latlng[1], 3)
   });
-  it("should have a description")
+  it("should have a description", function(){
+    assert.strictEqual(testLocation.description, "Galic name for New Scotland")
+  })
   it("should have events");
 
 
