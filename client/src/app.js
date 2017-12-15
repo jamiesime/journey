0e9dcc6d79c5bea2416ab24e82f5b27d7cdcdac4
Jamie Sime <jamiesime@gmail.com>
2017-12-15 10:33:52 +0000

    added makeRequest function to app.js

--- client/src/app.js
+++ client/src/app.js
@@ -5,4 +5,12 @@ var app = function(){
 
 }
 
+var makeRequest = function(url, callback){
+  request = new XMLHttpRequest();
+  request.open('GET', url);
+  request.addEventListener('load', callback);
+  request.send();
+};
+
+
 window.addEventListener("load", app);
