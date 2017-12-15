456757230bc52564f9be70869495cb5c6dc00091
Jamie Sime <jamiesime@gmail.com>
2017-12-15 10:35:30 +0000

    added requestJourney function to app.js - this will give json response which will populate locations and events

--- client/src/app.js
+++ client/src/app.js
@@ -12,5 +12,12 @@ var makeRequest = function(url, callback){
   request.send();
 };
 
+var requestJourney = function(){
+  if(this.status!=200){return};
+  var jsonString = this.responseText;
+  var countryList = JSON.parse(jsonString);
+}
+
+
 
 window.addEventListener("load", app);
