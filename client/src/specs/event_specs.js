14b5b7c5c55ce028730106bf11927b1ec1047971
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-12-15 10:41:23 +0000

    Make the code look a little bit cleaner

--- client/src/specs/event_specs.js
+++ client/src/specs/event_specs.js
@@ -6,7 +6,6 @@ describe( "Event", function(){
   var testEvent;
 
   beforeEach(function(){
-
     testEvent = new Event(
       "Battle of Montgomery's Tavern",
       "The Battle of Montgomery's Tavern was an incident in the Upper Canada Rebellion. The abortive revolutionary insurrection inspired by William Lyon Mackenzie was crushed by British authorities and Canadian volunteer units near a tavern on Yonge Street, Toronto.",
@@ -17,12 +16,15 @@ describe( "Event", function(){
     it("should have a name", function(){
       assert.strictEqual(testEvent.name, "Battle of Montgomery's Tavern")
     });
+
     it("should have a description", function(){
       assert.strictEqual(testEvent.description, "The Battle of Montgomery's Tavern was an incident in the Upper Canada Rebellion. The abortive revolutionary insurrection inspired by William Lyon Mackenzie was crushed by British authorities and Canadian volunteer units near a tavern on Yonge Street, Toronto.")
     });
+
     it("should have a date", function(){
       assert.strictEqual(testEvent.date, "7 December, 1837")
     });
+    
     it("should have a choices array", function(){
       assert.deepStrictEqual(testEvent.choices, ["Stay and Fight", "Flee to America with your Family"])
     });
