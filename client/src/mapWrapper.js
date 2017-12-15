4920301b17022fdab5a0121ed0f37bd71df24955
AMacFadyen <ferretsbite@hotmail.co.uk>
2017-12-15 10:33:31 +0000

    add marker function created

--- client/src/mapWrapper.js
+++ client/src/mapWrapper.js
@@ -2,4 +2,11 @@ MapWrapper = function(container, coords, zoom){
     this.googleMap = new google.maps.Map(container, center: coords, zoom: zoom
     });
     this.markers = [];
-}
+};
+
+MapWrapper.prototype.addMarker = function (coords) {
+    var marker = new google.maps.Marker({
+        position: coords,
+        content: owner
+    });
+};
