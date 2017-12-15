e4eb602f74e467cc1d89c1836489d2215a521884
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-12-15 10:26:05 +0000

    Initial tests set up

---
+++ client/src/specs/event_specs.js
@@ -0,0 +1,21 @@
+var assert = require("assert")
+var event = require("../event.js")
+
+describe( "Event", function(){
+
+  var testEvent;
+
+  beforeEach(function(){
+
+    testEvent = new Event();
+  });
+
+  it("should have a name");
+  it("should have a description");
+  it("should have a date");
+  it("should have a choices array")
+
+ // function(){
+ //    assert.strictEqual(recordStore.name, "Big Al's Records");
+ //  });
+});
