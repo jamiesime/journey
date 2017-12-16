f2ca1453c4faa52b9ece6af89fd3707386fa237d
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-12-16 11:02:12 +0000

    Location class -- get event details now working

--- client/src/specs/location_specs.js
+++ client/src/specs/location_specs.js
@@ -14,14 +14,15 @@ describe( "Location", function(){
       "Battle of Montgomery's Tavern",
       "The Battle of Montgomery's Tavern was an incident in the Upper Canada Rebellion. The abortive revolutionary insurrection inspired by William Lyon Mackenzie was crushed by British authorities and Canadian volunteer units near a tavern on Yonge Street, Toronto.",
       -4167676800,
-      ["Stay and Fight", "Flee to America with your Family"]);
-    });
+      ["Stay and Fight", "Flee to America with your Family"]
+    );
+
     event2 = new Event(
       "Random event",
       "Something happened",
       -4167676800,
-      ["Choice 1", "Choice 2"]);
-    });
+      ["Choice 1", "Choice 2"]
+    );
 
     testLocation = new Location("Nova Scotia", [44.651070, -63.582687], "Gaelic name for New Scotland", [event1, event2]);
   });
@@ -29,17 +30,29 @@ describe( "Location", function(){
   it("should have a name", function(){
     assert.strictEqual(testLocation.name, "Nova Scotia")
   });
+
   it("should have a lat", function(){
     assert.deepStrictEqual(testLocation.latlng[0], 44.651070)
   });
+
   it("should have a lng", function(){
     assert.deepStrictEqual(testLocation.latlng[1], -63.582687)
   });
+
   it("should have a description", function(){
     assert.strictEqual(testLocation.description, "Gaelic name for New Scotland")
   })
+
   it("should have events", function(){
     assert.strictEqual(testLocation.events.length, 2)
   });
+
+  it("should be able to get event details", function(){
+    assert.strictEqual(testLocation.events[0].name, "Battle of Montgomery's Tavern");
+  });
+
+  xit("should be able to add an event")
+  xit("should be able to remove an event");
+
 
 });
