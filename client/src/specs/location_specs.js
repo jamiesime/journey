45845e6d02c8859f9f529848e12f2541f9405548
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-12-16 10:53:30 +0000

    imported Event into Location class and set up two example event

--- client/src/specs/location_specs.js
+++ client/src/specs/location_specs.js
@@ -1,12 +1,29 @@
-var assert = require("assert")
-var Location = require("../location.js")
+var assert = require("assert");
+var Location = require("../location.js");
+var Event = require("../event.js");
 
 describe( "Location", function(){
 
-  var testLocation
+  var event1;
+  var event2;
+  var testLocation;
 
   beforeEach(function(){
-    testLocation = new Location("Nova Scotia", [44.651070, -63.582687], "Gaelic name for New Scotland", ["Event A", "Event B"]);
+
+    event1 = new Event(
+      "Battle of Montgomery's Tavern",
+      "The Battle of Montgomery's Tavern was an incident in the Upper Canada Rebellion. The abortive revolutionary insurrection inspired by William Lyon Mackenzie was crushed by British authorities and Canadian volunteer units near a tavern on Yonge Street, Toronto.",
+      -4167676800,
+      ["Stay and Fight", "Flee to America with your Family"]);
+    });
+    event2 = new Event(
+      "Random event",
+      "Something happened",
+      -4167676800,
+      ["Choice 1", "Choice 2"]);
+    });
+
+    testLocation = new Location("Nova Scotia", [44.651070, -63.582687], "Gaelic name for New Scotland", [event1, event2]);
   });
 
   it("should have a name", function(){
