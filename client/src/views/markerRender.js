09ce51b25e52ce9fb9f3ff3747a487ee59adb192
AMacFadyen <ferretsbite@hotmail.co.uk>
2017-12-16 11:00:38 +0000

    created moveLocation function

--- client/src/views/markerRender.js
+++ client/src/views/markerRender.js
@@ -6,12 +6,16 @@ MapView.prototype.render = function (markers) {
     console.log(markers);
 
     markers.forEach(function(marker){
-        mainMap = addMarker(lat: marker.latlng[0], lng: marker.latlng[1]);
+        mainMap = addMarker(lat: marker.Latlng[0], lng: marker.Latlng[1]);
     });
 };
 
 MapView.prototype.moveLocation = function (location) {
-
+    var lat = location.Latlng[0];
+    var lng = location.Latlng[1];
+    var newCoords ={lat: lat, lng: lng};
+    mainMap.addMarker(newCoords, location.placeName);
+    mainMap.repositionMap(newCoords, 7);
 };
 
 module.exports = MapView;
