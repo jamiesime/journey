758d881baf780223584f1cf4c0bc959bc7adcbb0
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-12-18 19:33:01 +0000

    TestDice set up

---
+++ client/src/specs/dice_spec.js
@@ -0,0 +1,13 @@
+var assert = require("assert");
+var Die = require("../Die.js");
+var Dice = require("../Dice.js");
+
+describe( "Dice Roll", function(){
+  var dice;
+
+  beforeEach(function(){
+    dice = new Dice();
+  });
+
+  it("return 5 random numbers")
+});
