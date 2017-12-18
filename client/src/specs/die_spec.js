0ec70963fbe62fd0f573f3b1a1e3626738b8f1cc
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-12-18 19:25:44 +0000

    set up die test file

---
+++ client/src/specs/die_spec.js
@@ -0,0 +1,12 @@
+var assert = require("assert");
+var Die = require("../Die.js");
+
+describe( "Die Roll", function(){
+  var die;
+
+  beforeEach(function(){
+    die = new Die();
+  });
+
+  it("return a random number")
+});
