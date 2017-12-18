108ee0f127fdf4a552bd61067da2ab3ce0e3dfab
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-12-18 19:42:13 +0000

    Added four new tests for the dice_spec

--- client/src/specs/dice_spec.js
+++ client/src/specs/dice_spec.js
@@ -12,4 +12,10 @@ describe( "Dice Roll", function(){
   it("return 5 random numbers", function(){
     assert.deepStrictEqual(dice.rollDice().length, 5);
   });
+
+  it("should, before rolling, have shipExist set to false");
+  it("should, before rolling, have captExist set to false");
+  it("should, before rolling, have crewExist set to false");
+  it("should, before rolling, have cargo set to zero");
+
 });
