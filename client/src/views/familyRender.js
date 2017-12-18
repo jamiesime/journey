bc757ba4d3af01b8aa11c7859302f0a10b1254e4
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-12-18 11:49:56 +0000

    health bar showing

--- client/src/views/familyRender.js
+++ client/src/views/familyRender.js
@@ -8,7 +8,7 @@ FamilyRender.prototype = {
     container = document.getElementById("sidebar-info-content");
     container.innerHTML = "";
     var header = document.createElement("h2");
-    header.innerText = document.getElementById('userInput').value;
+    header.innerText = document.getElementById('userInput').value + " family";
     if(document.getElementById('userInput').value.length === 0){
       header.innerText = "Generic Family"
     }
@@ -31,6 +31,17 @@ var renderEachMember = function(family){
   family.members.forEach(function(member){
     memberBox = document.createElement("div");
     memberBox.classList += "member-box";
+    var healthBarContainer = document.createElement("div");
+    var healthBar = document.createElement("div");
+    healthBarContainer.style.position = "relative";
+    healthBarContainer.style.backgroundColor = "tomato";
+    healthBar.style.position = "absolute";
+    healthBar.style.backgroundColor = "green";
+    healthBarContainer.style.height = "20px";
+    healthBar.style.height = "20px";
+    healthBarContainer.style.width = "100px";
+    healthBar.style.width = "110px";
+
     var sprite = document.createElement("img");
     sprite.src = "./images/" + member.name + ".png";
     sprite.classList += "family-sprite";
@@ -40,10 +51,13 @@ var renderEachMember = function(family){
     age.innerText = "Age: " + member.age;
     var health = document.createElement("p");
     health.innerText = "Health: " + member.health;
+
+    healthBarContainer.appendChild(healthBar);
     memberBox.appendChild(sprite);
     memberBox.appendChild(name);
     memberBox.appendChild(age);
     memberBox.appendChild(health);
+    memberBox.appendChild(healthBarContainer);
     familyContainer.appendChild(memberBox);
   })
 }
