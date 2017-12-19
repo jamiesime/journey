4d1574e1c6367abc2068d1f683ddcb2d750b93ee
LouiseReid <l_reid@hotmail.co.uk>
2017-12-19 15:51:05 +0000

    la prairie script edit

--- client/src/seeds.js
+++ client/src/seeds.js
@@ -129,28 +129,29 @@ db.locations.insert({
 db.locations.insert({
     placeName: "La Prairie, Quebec",
     Latlng: [ 45.41678, -73.49917],
-    description: "July 21, 1836, a wood-burning steam locomotive chugged out of La Prairie, Quebec, pulling the first train on the first public railroad in Canada. Its first official run was held with great fanfare. The locomotive pulled two first-class coaches carrying thirty-two dignitaries, including Lord Gosford, the governor general of Lower Canada. A second train pulled by a team of horses followed close behind. Two hours later, the trains arrived in St. John to a rousing welcome.",
+    description: "July 21, 1836, a wood-burning steam locomotive chugged out of La Prairie, Quebec, pulling the first train on the first public railroad in Canada. Its first official run was held with great fanfare. \nThe locomotive pulled two first-class coaches carrying thirty-two dignitaries, including Lord Gosford, the governor general of Lower Canada. \nA second train pulled by a team of horses followed close behind. Two hours later, the trains arrived in St. John to a rousing welcome.",
     events:
       [{
           date: 1836,
-          text: "The railway served as a way for those travelling between Montreal and New York to avoid a bumpy stagecoach journey that bypassed a series of difficult rapids on the Richelieu. At Saint-Jean, passengers transferred to a steamer that carried them south to New York City via Lake Champlain and the Hudson River. \n\nThe Lower Canada Rebellion, commonly referred to as The Patriots War by Qubecers is the name given to the armed conflict in 1837-1838 between the rebels of lower Canada and the British colonial power of the province.",
+          text: "The railway served as a way for those travelling between Montreal and New York to avoid a bumpy stagecoach journey that bypassed a series of difficult rapids on the Richelieu. At Saint-Jean, passengers transferred to a steamer that carried them south to New York City via Lake Champlain and the Hudson River. \n\nThe Lower Canada Rebellion, commonly referred to as The Patriots War by Qubecers is the name given to the armed conflict in 1837-1838 between the rebels of lower Canada and the British colonial power of the province. \n\n A fellow railway worker Pierre asks you to join him in fighting with the Rebels.",
           choices:
           [
             { text: "Travel to New York",
-              goto: [7, 0]},
+              goto: [6, 0]},
               { text: "Join the Rebellion",
-               goto: [6, 1],
-               memberHealthChange: {name: "John", change: -40, source: "fighting in Rebellion"}
+               goto: [5, 1],
+               memberHealthChange: {name: "John", change: -40, source: "fighting in Rebellion"},
+               memberAdd: {name: "Pierre", born: 1810, health: 70}
            }
           ]
       },
       {
         date: 1837,
-        text: "The railway served as a way for those travelling between Montreal and New York to avoid a bumpy stagecoach journey that bypassed a series of difficult rapids on the Richelieu. At Saint-Jean, passengers transferred to a steamer that carried them south to New York City via Lake Champlain and the Hudson River.",
+        text: "The railway served as a way for those travelling between Montreal and New York to avoid a bumpy stagecoach journey that bypassed a series of difficult rapids on the Richelieu. At Saint-Jean, passengers transferred to a steamer that carried them south to New York City via Lake Champlain and the Hudson River.\n\nDue to your labour on the tracks you and your family are given a free pass to New York.",
         choices:
         [
           { text: "Travel to New York",
-            goto: [7, 0]
+            goto: [6, 0]
           }
         ]
       }
