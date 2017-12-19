65972106f306be9b65a0edb94025aaa1faf22582
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-12-19 14:37:45 +0000

    dice images now showing

--- client/src/views/diceGameRender.js
+++ client/src/views/diceGameRender.js
@@ -35,15 +35,31 @@ DiceGameRender.prototype = {
     rollBtn.addEventListener('click', function(){
       game.rollDice();
       game.computerRollDice();
-      var reel1 = document.getElementById('reel1')
-      var reel2 = document.getElementById('reel2')
-      var reel3 = document.getElementById('reel3')
-      var reel4 = document.getElementById('reel4')
-      var reel5 = document.getElementById('reel5')
+      var reel1 = document.getElementById('reel1');
+      var reel2 = document.getElementById('reel2');
+      var reel3 = document.getElementById('reel3');
+      var reel4 = document.getElementById('reel4');
+      var reel5 = document.getElementById('reel5');
+      var cpureel1 = document.getElementById('cpu-reel1');
+      var cpureel2 = document.getElementById('cpu-reel2');
+      var cpureel3 = document.getElementById('cpu-reel3');
+      var cpureel4 = document.getElementById('cpu-reel4');
+      var cpureel5 = document.getElementById('cpu-reel5');
 
-      console.log(reel1.innerText);
+      var dice1 = document.getElementById('dice1')
+      dice1.src = "./images/" + reel1.innerText + ".png";
+      var dice2 = document.getElementById('dice2')
+      dice2.src = "./images/" + reel2.innerText + ".png";
+      var dice3 = document.getElementById('dice3')
+      dice3.src = "./images/" + reel3.innerText + ".png";
+      var dice4 = document.getElementById('dice4')
+      dice4.src = "./images/" + reel4.innerText + ".png";
+      var dice5 = document.getElementById('dice5')
+      dice5.src = "./images/" + reel5.innerText + ".png";
+
+
+
     });
-    // dice.src = "./images/" + reel.value + ".png";
   }
 
 }
@@ -55,14 +71,14 @@ var renderReelandHolds = function(container, reelId, holdId){
     var checkbox = document.createElement("input");
     checkbox.id = holdId + (i+1);
     checkbox.type = "checkbox";
-    var dice = document.createElement("img");
+    dice = document.createElement("img");
     dice.classList += "dice-face"
-    dice.id = "dice" + (i+1);
-    console.log(reel);
+    dice.id = "dice"+(i+1)
+
 
     container.appendChild(reel);
     container.appendChild(checkbox);
-    // container.appendChild(dice);
+    container.appendChild(dice);
   }
 }
 
