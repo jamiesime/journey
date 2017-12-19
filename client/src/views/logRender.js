3ae547a9fbb6e0205f6e34e67d117c6f56468803
James Stewart <james.stewart.06@aberdeen.ac.uk>
2017-12-19 11:25:29 +0000

    log render complete && dice faces uploaded for dice game

--- client/src/views/logRender.js
+++ client/src/views/logRender.js
@@ -9,12 +9,13 @@ LogRender.prototype = {
     container = document.getElementById("sidebar-info-content");
     container.innerHTML = "";
     header = document.createElement("h2");
-    header.innerText = "Recent Events";
+    header.innerText = "Diary";
     container.appendChild(header);
     if (loggedEvents != null || loggedEvents != undefined){
       loggedEvents.forEach(function(event){
         var logItem = document.createElement("p");
         logItem.innerText = event.eventText;
+        logItem.style.fontFamily = "Coronetscript, cursive"
         var hr = document.createElement('hr');
         container.appendChild(logItem);
         container.appendChild(hr);
